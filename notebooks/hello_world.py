1fab9ab4d80dfa499267012fdae7868ee0154d6b
clementjumel <clement.jumel@gmail.com>
2023-06-05 15:06:05 +0200

    ✨ Basic python package structure

---
+++ notebooks/hello_world.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from plathon.hello_world import hello_world
+
+hello_world()
+
+# %%
---
+++ tests/hello_world_test.py
@@ -0,0 +1,5 @@
+from plathon.hello_world import hello_world
+
+
+def test_hello_world() -> None:
+    hello_world()
